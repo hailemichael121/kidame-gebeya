f47b8c9d4a16a90b8158a7e2a74185bfa5117d94
hailemichael121 <yihunaashe@gmail.com>
2024-03-06 05:10:27 +0300

    added catagories section, latest arrival section and product display section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Categories, LatestItemDisplay, ProductDisplay } from "../components";
 import ScrollableImageGrid from "../components/scrollableImageGrid/ScrollableImageGrid";
+import { NavBar } from "../components";
 
 const HomePage = () => {
   return (
     <>
+      <NavBar />
       <ScrollableImageGrid />
       <Categories />
       <LatestItemDisplay />
